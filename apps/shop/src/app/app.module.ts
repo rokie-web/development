a2d0ad480dcefc224c7d0ebe636afb0640e3a3af
Robert Kiemele <rokie@Roberts-MBP.fritz.box>
2020-06-11 15:22:41 +0200

    added pizza app

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './containers/app.component';
 import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
