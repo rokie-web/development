c9d3da4f38f9f9f2c74065f44ee81c14b664061c
Robert Kiemele <rokie@Roberts-MBP.fritz.box>
2020-06-11 17:17:31 +0200

    added pizza reducer

---
+++ apps/shop/src/products/store/reducers/pizzas.reducer.ts
@@ -0,0 +1,42 @@
+import { createReducer, on } from '@ngrx/store';
+
+import {
+  LOAD_PIZZAS,
+  LOAD_PIZZAS_SUCCESS,
+  LOAD_PIZZAS_FAIL,
+} from '../actions/pizzas.action';
+import { Pizza } from '../../models/pizza.model';
+
+export interface PizzaState {
+  data: Pizza[];
+  loaded: boolean;
+  loading: boolean;
+}
+
+export const INITIAL_STATE = {
+  data: [],
+  loaded: false,
+  loading: false,
+};
+
+const _pizzaReducer = createReducer(
+  INITIAL_STATE,
+  on(LOAD_PIZZAS, (state) => ({
+    ...state,
+    loading: true,
+  })),
+  on(LOAD_PIZZAS_SUCCESS, (state) => ({
+    ...state,
+    loaded: true,
+    loading: false,
+  })),
+  on(LOAD_PIZZAS_FAIL, (state) => ({
+    ...state,
+    loaded: true,
+    loading: false,
+  }))
+);
+
+export function pizzaReducer(state, action): PizzaState {
+  return _pizzaReducer(state, action);
+}
