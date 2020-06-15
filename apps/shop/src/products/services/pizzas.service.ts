4636cd501728ac4f54b6e0f7fcb94922f511bfae
Robert Kiemele <rokie@Roberts-MBP.fritz.box>
2020-06-15 15:09:34 +0200

    removed error handling

--- apps/shop/src/products/services/pizzas.service.ts
+++ apps/shop/src/products/services/pizzas.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 
 import { Pizza } from '../models/pizza.model';
 
@@ -14,21 +13,17 @@ export class PizzasService {
 
   getPizzas(): Observable<Pizza[]> {
     return this.http.get<Pizza[]>(`${URL}/pizzas`);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   createPizza(payload: Pizza): Observable<Pizza> {
     return this.http.post<Pizza>(`${URL}/pizzas`, payload);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   updatePizza(payload: Pizza): Observable<Pizza> {
     return this.http.put<Pizza>(`${URL}/pizzas/${payload.id}`, payload);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   removePizza(payload: Pizza): Observable<Pizza> {
     return this.http.delete<any>(`${URL}/pizzas/${payload.id}`);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 }
