281fe98d8288780be63d58bb96e8c58623e5e86d
Robert Kiemele <rokie@Roberts-MBP.fritz.box>
2020-07-03 15:17:36 +0200

    Simplified service for good

--- apps/shop/src/products/services/pizzas.service.ts
+++ apps/shop/src/products/services/pizzas.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
-import { Observable, throwError, EMPTY } from 'rxjs';
-import { catchError, share } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { Pizza } from '../models/pizza.model';
 
@@ -18,16 +17,13 @@ export class PizzasService {
 
   createPizza(payload: Pizza): Observable<Pizza> {
     return this.http.post<Pizza>(`${URL}/pizzas`, payload);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   updatePizza(payload: Pizza): Observable<Pizza> {
     return this.http.put<Pizza>(`${URL}/pizzas/${payload.id}`, payload);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 
   removePizza(payload: Pizza): Observable<Pizza> {
     return this.http.delete<any>(`${URL}/pizzas/${payload.id}`);
-    //.pipe(catchError((error: any) => Observable.throw(error.json())));
   }
 }
